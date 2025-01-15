9c863e201b362f3c416150a134ca35a41316173d
KLEBERPAZ <kleberpaz@hotmail.com>
2025-01-15 00:03:26 -0300

    teste

--- meu_site.py
+++ meu_site.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session 
 from datetime import datetime
 import os
+from fpdf import FPDF
 
 app = Flask(__name__)
 app.secret_key = 'sua_chave_secreta_aqui'  # Substitua por uma chave secreta segura
